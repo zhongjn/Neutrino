4a184cf9ff1d557b4230a32bc4672fba300d8936
zhongjn <2389206378@qq.com>
2019-04-23 13:26:47 +0800

    sqlite

--- CredentialInfo.cpp
+++
@@ -1,8 +0,0 @@
-#include "CredentialInfo.h"
-
-CredentialInfo::CredentialInfo(MailAddress username, string password, string server, int port) {
-	m_Username = username;
-	m_Password = password;
-  m_Server = server;
-  m_Port = port;
-}
--- Mail.cpp
+++
@@ -1,8 +0,0 @@
-#include "Mail.h"
-
-Mail::Mail(string subject, string content, MailAddress sender, MailAddress receiver) {
-	m_Subject = subject;
-	m_Content = content;
-	m_Sender = sender;
-	m_Receiver = receiver;
-}
