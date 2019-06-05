97fe926075f3cbe939177cf338c02a75329ebbb6
zhongjn <2389206378@qq.com>
2019-06-05 20:55:37 +0800

    some test code

--- Main.cpp
+++ Main.cpp
@@ -1,29 +1,24 @@
-/*
-#include "MailManager.h"
-#include <iostream>
-#include "ThirdParty/sqlite3/sqlite3.h"
-
-using namespace std;
-
-int main() {
-    // TODO: 进入UI函数
-    // UI负责操作MailManager
-    MailManager mgr;
-
-    return 0;
-}
-*/
 #include "UI/mainwindow.h"
 #include "UI/send.h"
 #include "UI/welcome.h"
 #include "UI/inbox.h"
 #include <QApplication>
 
+int RunQt(int argc, char* argv[]) {
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
+
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-	MainWindow w;
-	w.show();
+  
 
-	return a.exec();
+    MailManager mgr;
+    // test code
+
+    return RunQt(argc, argv);
+
 }
